827838347f84d723a365c57f1e6241217ca9a862
Camila <camiart@gmail.com>
2023-07-29 20:02:21 -0300

    exercicio03

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 
+import { useState } from 'react'
 import './App.css'
 import CardAdicionar from './components/CardAdicionar'
 
 function App() {
+
+  const [listaTarefas, setListaTarefas] = useState([
+    {textoTarefa: "Tarefa 1", finalizada: false},
+  ])
   
 
   return (
