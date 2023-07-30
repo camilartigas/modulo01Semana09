661e3765794e139f5bff5173bcd26d3a042fd1b8
Camila <camiart@gmail.com>
2023-07-29 21:16:53 -0300

    exercicio04

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 
 import { useState } from 'react'
 import './App.css'
-import CardAdicionar from './components/CardAdicionar'
+import CardAdicionar from './components/cardAdicionar'
 
 function App() {
 
-  const [listaTarefas, setListaTarefas] = useState([
-    {textoTarefa: "Tarefa 1", finalizada: false},
-  ])
+  const [listaTarefas, setListaTarefas] = useState([])
+
+  const adicionarTarefa = (texto) => {
+    if(!texto) {
+      alert("Digite uma tarefa")
+      return
+    }
+    const novaTarefa = {id: listaTarefas.length +1, textoTarefa: texto, finalizada: false}
+
+    setListaTarefas([...listaTarefas, novaTarefa])
+  }
   
 
   return (
-    < >
-      <CardAdicionar/>
+    <>
+      <CardAdicionar adicionarTarefa={adicionarTarefa}/>
+      <div>{listaTarefas.map(tarefa => (<span>{tarefa.textoTarefa}</span>))}</div>
     </>
   )
 }
--- src/components/CardAdicionar/index.jsx
+++ src/components/CardAdicionar/index.jsx
@@ -1,13 +1,12 @@
 import {useState} from 'react'
-function CardAdicionar(){
+function CardAdicionar(props){
 
   //o useState tem duas variáveis, a primeira é o valor da variável e a segunda é a função que altera o valor da variável, tendo um jeito de guardar o texto da tarefa
   const [textoTarefa, setTextoTarefa] = useState(" ")
   return (
     <div>
-      {/* <h1>Texto da tarefa: {textoTarefa}</h1> */}
       <input type="text" value={textoTarefa} onChange={ (evento) => setTextoTarefa(evento.target.value)} placeholder="Digite sua tarefa"/>
-      <button onClick={()=>{}}>Adicionar</button>
+      <button onClick={()=> props.adicionarTarefa(textoTarefa)}>Adicionar</button>
     </div>
     
   )
